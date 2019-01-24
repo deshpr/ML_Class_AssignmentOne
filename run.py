c4229e618b344f11343105336ca0ac91794bd99a
Rahul Deshpande <radeshpa@microsoft.com>
2019-01-24 07:47:29 -0800

    change

--- run.py
+++ run.py
@@ -6,43 +6,22 @@ from sklearn import metrics
 from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
 import pdb
+from operator import add
+from sklearn.model_selection import KFold # import KFold
+import sys
 
-def calculate_coefficients(X_train, Y_train):
-    # get data as a series
-    X_train_series = X_train[X_train.columns[0]]
-    print("input type, x = {}, y  = {}".format(type(X_train_series), type(Y_train)))
-    mean_x = X_train_series.mean()
-    mean_y = Y_train.mean()
-    print("mean x = {}, mean y = {}".format(mean_x, mean_y))
-    mean_product = mean_x * mean_y
-    print("sum")
-    sum_product = (Y_train * X_train_series).sum()
-    count = len(X_train_series)
 
-    X_Mean_Squared = mean_x * mean_x
-    X_Squared_Sum = (X_train_series ** 2).sum()
-
-    Coefficient_One = sum_product - (count * mean_x * mean_y)
-    Coefficient_Two = X_Squared_Sum - (count * X_Mean_Squared)
-
-    thetaOne = Coefficient_One/Coefficient_Two
-    thetaZero = mean_y - (thetaOne*mean_x)
-
-    return thetaZero, thetaOne
-
-def linear_regression(X_train, Y_train):
-    thetaZero, thetaOne = calculate_coefficients(X_train, Y_train)
-    Y_prediction =  thetaZero + (thetaOne * X_train)
-    # plotting the regression line 
-    plt.plot(X_train, Y_prediction, color = "g") 
-    plt.scatter(X_train, Y_train)
-    # putting labels 
-    plt.xlabel('x') 
-    plt.ylabel('y') 
-    # function to show plot 
-    plt.show() 
 
 def calculateMseCost(X, Y, theta):
+    """Given the input data and the actual output, and the theta, calculates the Mean Squared Error
+        between the predictions and the actual values. Predictions are calculated by performin a dot product
+        between X and theta.
+
+        Keyword arguments:
+        X -- A data frame, representing the input data set that has the data for which we will perform the prediction.
+        Y -- A series, that has the actual values for every element in the data set.
+        theta -- The coefficients that are used to calculate the predictions.
+    """
     count = len(X)        
     X_transformed = X.copy()
     X_transformed.insert(0, 'for theta 0', 1)
@@ -51,22 +30,16 @@ def calculateMseCost(X, Y, theta):
     cost = (1/1)*(1/count) * np.sum(difference)
     return cost
 
+def gradient_descent_matrix(X_train, Y_train, alpha):    
+    """Performs Batch Gradient descent for Linear Regression, by employing the matrix approach.
+        The algorithm continues to take steps until either the difference in theta becomes less than 0.000001, or
+        1 million iterations have been performed.
 
-def calculateCostIterative(thetaZero, thetaOne, X, Y):
-    total = 0
-    for i in range(0, len(X)):
-#        pdb.set_trace()
-        yValue = Y.iloc[i]
-        xValue = X.iloc[i,0]
-        prediction = xValue * thetaOne + thetaZero
-        error = prediction - yValue
-        total += (error * error)
-    return total/(2 * len(X))
-
-def getPredictions(X, theta):
-    return np.dot(X, theta)
-
-def gradient_descent_matrix(X_train, Y_train, alpha, iterationCount):    
+        Keyword Arguments:
+        X_train : A data frame that has the training data.
+        Y_train: A data frame that has the expected output for the training data.
+        alpha: The learning rate.
+    """
     X_train_transformed = X_train.copy()
     X_train_transformed.insert(0, 'for theta 0', 1)
     featureCount = len(X_train_transformed.columns)
@@ -90,8 +63,18 @@ def gradient_descent_matrix(X_train, Y_train, alpha, iterationCount):
     print("Theta = {}".format(thetaInitial))
     return thetaInitial
 
-
 def iterative_step_gradient_descent(X_train, Y_train, thetaZeroStart, thetaOneStart, alpha):
+    """Performs a single step of gradient descent using the iterative approach.
+        Calculates the partial derivatives for both thetaZero and thetaOne, and then returns the updated thetaZero 
+        and thetaOne values.
+
+        Keyword Arguments:
+        X_train: The input training data with the features and their values.
+        Y_train: The expected output for the training data.
+        thetaZeroStart: The first coefficient
+        thetaOneStart: The second coefficient
+        alpha: The learning rate.
+    """
     # Add it in the beginning
     count = len(X_train)
     thetaZeroGradient = 0
@@ -106,7 +89,18 @@ def iterative_step_gradient_descent(X_train, Y_train, thetaZeroStart, thetaOneSt
     newThetaOne = thetaOneStart - (alpha * (1/count) * thetaOneGradient) 
     return [newThetaZero, newThetaOne]
 
-def gradient_descent(X_train, Y_train, thetaZero, thetaOne, alpha, iterationCount):
+def iterative_gradient_descent(X_train, Y_train, thetaZero, thetaOne, alpha):
+    """Performs the iterative gradient descent for  linear regression.
+        After performing multiple iterations, this method returns the coefficients for the 
+        linear regression model for the given training data.
+
+        Keyword Arguments:
+        X_train: The input training data
+        Y_train: The expected output for the input training data.
+        thetaZero: The first coefficient (this is the intercept for the linear regression model.)
+        thetaOne : The second coefficient for linear regression.
+
+    """
     i = -1
     while True:
         i += 1
@@ -121,6 +115,16 @@ def gradient_descent(X_train, Y_train, thetaZero, thetaOne, alpha, iterationCoun
     return [thetaZero, thetaOne]
 
 def calculateSkLearnGradientDescent(X_train, y_train, X_test, y_test, feature_cols):
+    """Performs Linear Regression using the sklearn package.
+        Returns the coefficients for the linear regression model after fitting the input training data.
+
+        Keyword Arguments:
+        X_train: The input training data
+        Y_train: The expected output for the input training data.
+        X_test: The test data with the features
+        y_test: The test data with the actual ouput values.
+        feature_cols: The actual values
+    """
     # Fitting the model
     linreg = LinearRegression()
     linreg.fit(X_train, y_train)
@@ -132,97 +136,118 @@ def calculateSkLearnGradientDescent(X_train, y_train, X_test, y_test, feature_co
 
     return [linreg.intercept_, linreg.coef_[0]]
 
-def mean_squared_error(y_actual,  y_test):
-    difference = y_actual - y_test
-    squared = difference**2
-    length = len(squared)
-    print("length = {}".format(length))
-    mean = squared.sum()/length
-    print("mean  = {}".format(mean))
-    return mean
+
+def plot_data(x, y):
+    """Plots the data as a scatter plot. Also both the inputs must be data frames.
+    """
+    plt.scatter(x, y)
+    plt.show() # Depending on whether you use IPython or interactive mode, etc.
+    data.plot(style=['o','rx'])
 
 
-filename = "data/insurance.csv"
-data = pd.read_csv(filename)
-print("finished reading the data")
-print(data.columns.values)
-# Preparing the train and test data
-feature_cols = ['X']
-X = data[feature_cols]
-y = data['Y']
-print("type, x = {}, y = {}".format(type(X), type(y)))
-print("splitting the data")
-from sklearn.model_selection import KFold # import KFold
-foldCount = 3
-kf = KFold(n_splits=foldCount)
-for train_index, test_index in kf.split(X):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-thetaZeroStart = 0
-thetaOneStart = 0
-alpha = 0.001
-iterationCount = 100000
-
-# 3 different ways of calculating our results.
-
-thetaFromIterative = gradient_descent(X_train, y_train, thetaZeroStart, thetaOneStart, alpha, iterationCount)
-thetaFromMatrix = gradient_descent_matrix(X_train, y_train, alpha, iterationCount)
-thetaFromSkitLearn = calculateSkLearnGradientDescent(X_train, y_train, X_test, y_test, feature_cols)
-
-print("Theta from skit learn approach = {}, type = {}".format(thetaFromSkitLearn, type(thetaFromSkitLearn)))
-print("Theta from iterative approach = {}, type = {}".format(thetaFromIterative, type(thetaFromIterative)))
-print("Theta from matrix approach = {}, type = {}".format(thetaFromMatrix, type(thetaFromMatrix)))
+def main(filename):
+    print("Reading the data..")
+    data = pd.read_csv(filename)
+    print("Finished reading the data..")
+    # Preparing the train and test data
+    feature_cols = ['X']
+    X = data[feature_cols]
+    y = data['Y']
+    print("Splitting the data using k fold approach, with k = 3")
 
 
-print("Test Theta from skit learn")
-cost = calculateMseCost(X_test, y_test, np.array(thetaFromSkitLearn))
-print("cost  = {}".format(cost))
+    foldCount = 3
 
-print("Test Theta from iterative")
-cost = calculateMseCost(X_test, y_test, np.array(thetaFromIterative))
-print("cost  = {}".format(cost))
-print("Test Theta from matrix approach")
-cost = calculateMseCost(X_test, y_test, np.array(thetaFromMatrix))
-print("cost  = {}".format(cost))
+    # To perform cross validation.
+
+    kf = KFold(n_splits=foldCount)
+
+    averageMseIterative = 0
+    averageMseMatrix = 0
+    averageMseSkLearn = 0
+
+    averageThetaIterative = np.array([0,0])
+    averageThetaMatrix = np.array([0,0])
+    averageThetaSkLearn = np.array([0,0])
+
+    iteration = 0
+    for train_index, test_index in kf.split(X):
+        print("-------------------------------------------------------------------------------------------------------")
+        print("\n")
+        print("At iteration {} of testing.".format(iteration))
+        iteration += 1
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        thetaZeroStart = 0
+        thetaOneStart = 0
+        alpha = 0.001
+        iterationCount = 100000
+
+        # 3 different ways of calculating our results.
+
+        thetaFromIterative = iterative_gradient_descent(X_train, y_train, thetaZeroStart, thetaOneStart, alpha)
+        averageThetaIterative =  averageThetaIterative + np.array(thetaFromIterative)
+
+        thetaFromMatrix = gradient_descent_matrix(X_train, y_train, alpha)
+        averageThetaMatrix = averageThetaMatrix + np.array(thetaFromMatrix)
+
+        thetaFromSkitLearn = calculateSkLearnGradientDescent(X_train, y_train, X_test, y_test, feature_cols)
+        averageThetaSkLearn = averageThetaSkLearn + np.array(thetaFromSkitLearn)
+
+        print("Theta from skit learn approach = {}".format(thetaFromSkitLearn, type(thetaFromSkitLearn)))
+        print("Theta from iterative approach = {}".format(thetaFromIterative, type(thetaFromIterative)))
+        print("Theta from matrix approach = {}".format(thetaFromMatrix, type(thetaFromMatrix)))
+        print("\n")
+
+        cost = calculateMseCost(X_test, y_test, np.array(thetaFromSkitLearn))
+        print("MSE Cost using Sk Learn = {}".format(cost))
+        averageMseSkLearn += cost
+
+        cost = calculateMseCost(X_test, y_test, np.array(thetaFromIterative))
+        print("MSE Cost from Iterative Approach = {}".format(cost))
+        averageMseIterative += cost
+
+        cost = calculateMseCost(X_test, y_test, np.array(thetaFromMatrix))
+        print("MSE Cost from matrix approach  = {}".format(cost))
+        averageMseMatrix += cost
+
+    # Calculate the average.
+
+    averageMseIterative /= 3
+    averageMseSkLearn /= 3
+    averageMseMatrix /= 3
+
+    averageThetaIterative  = averageThetaIterative/3
+    averageThetaSkLearn = averageThetaSkLearn/3
+    averageThetaMatrix = averageThetaMatrix/3
+
+    print("---------------------------------------------------------------------------------------------------------")
+    print("Final Results...\n\n")
+    print("Theta:")
+    print("Theta SkLearn: {}".format(averageThetaSkLearn))
+    print("Theta Matrix: {}".format(averageThetaMatrix))
+    print("Theta Iterative: {}".format(averageThetaIterative))
+    print("\n")
+    print("MSE")
+    print("MSE SkLearn: {}".format(averageMseSkLearn))
+    print("MSE Matrix: {}".format(averageMseMatrix))
+    print("MSE Iterative: {}".format(averageMseIterative))    
+    print("Program complete..")
+
+
+if __name__ == '__main__':    
+    print('Number of arguments:', len(sys.argv), 'arguments.')
+    print('Argument List:', str(sys.argv))
+    filename = sys.argv[1]
+    main(filename)
 
 
 
-"""
-# Data
-plt.scatter(data['X'], data['Y'])
-plt.show() # Depending on whether you use IPython or interactive mode, etc.
-data.plot(style=['o','rx'])
-
-"""
-
-
-# Evaluating the model
-#print("RMSE:", np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-#print("Length = {}, {}".format(len(y_pred), len(y_test)))
-"""
-
-mse = mean_squared_error(y_test, y_pred)
-print("mse = {}".format(mse))
 
 
 
-print("##########################################################")
-print("POLYNOMIAL REGRESSION!!!!!")
-poly=PolynomialFeatures(degree=4)
-poly_X=poly.fit_transform(X)
-# create a Linear Regressor   
-lin_regressor = LinearRegression()
-# fit this to Linear Regressor
-lin_regressor.fit(X_train,y_train)
-y_preds = lin_regressor.predict(X_test)
-print("\nMAE:", metrics.mean_absolute_error(y_test, y_pred))
-print("MSE:", metrics.mean_squared_error(y_test, y_pred))
-print("RMSE:", np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-"""
+
 
 
 
